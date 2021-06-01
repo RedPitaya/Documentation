ac789c26c25f701a4496212ab9628233725d0842
Nikolaj P. Danilyuk <n.danilyuk@integrasources.com>
2021-06-01 14:17:33 +0700

    Modify conf.py

--- conf.py
+++ conf.py
@@ -39,6 +39,8 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
+sphinx_tabs_valid_builders = ['linkcheck']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
