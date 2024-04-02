cb7a5ea55a551e8fef03fb249e76864cc34d2b0c
Lightsaver7 <94392770+Lightsaver7@users.noreply.github.com>
2024-04-02 15:08:19 +0200

    2.00-35

--- conf.py
+++ conf.py
@@ -86,7 +86,7 @@ author = "Red Pitaya"
 # built documents.
 #
 # The short X.Y version.
-version = "2.00-30"
+version = "2.00-35"
 # The full version, including alpha/beta/rc tags.
 release = version
 
